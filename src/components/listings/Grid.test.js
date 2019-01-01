40cd817db6310d0f94aceed9a6e0f917275788e6
Cavan Wagg <waggx002@gmail.com>
2019-01-01 16:28:17 -0600

    organize tests with describe

--- src/components/listings/Grid.test.js
+++ src/components/listings/Grid.test.js
@@ -2,39 +2,46 @@ import data from '../../data/courses.json';
 
 const numItems = data.length;
 
-test('Number of Items = 12', () => {
-  expect(numItems).toBe(12);
-});
-// Numbers
-test('Number of Items to be greater than 12', () => {
-  expect(numItems).toBeGreaterThanOrEqual(12);
-});
+describe('Number tests', () => {
+  // Numbers
+  test('Number of Items = 12', () => {
+    expect(numItems).toBe(12);
+  });
 
-// Strings
-const dataTest = data[0].title;
-
-test('There is a JS in this title', () => {
-  expect(dataTest).toMatch(/JS/);
+  test('Number of Items to be greater than 12', () => {
+    expect(numItems).toBeGreaterThanOrEqual(12);
+  });
 });
 
-test('The title contains React', () => {
-  expect(dataTest).toContain('React');
+describe('String tests', () => {
+  // Strings
+  const dataTest = data[0].title;
+
+  test('There is a JS in this title', () => {
+    expect(dataTest).toMatch(/JS/);
+  });
+
+  test('The title contains React', () => {
+    expect(dataTest).toContain('React');
+  });
 });
 
-// Arrays
-const data2 = ['React Native', 'MeteorJS'];
+describe('Arrays and Objects tests', () => {
+  // Arrays
+  const data2 = ['React Native', 'MeteorJS'];
 
-test('The list of course mentions React native and MeteorJS ', () => {
-  expect(['React Native', 'MeteorJS', 'React']).toEqual(
-    expect.arrayContaining(data2)
-  );
-});
+  test('The list of course mentions React native and MeteorJS ', () => {
+    expect(['React Native', 'MeteorJS', 'React']).toEqual(
+      expect.arrayContaining(data2)
+    );
+  });
 
-// Objects
-test('The first course to have a property of Title', () => {
-  expect(data[0]).toHaveProperty('title');
-});
+  // Objects
+  test('The first course to have a property of Title', () => {
+    expect(data[0]).toHaveProperty('title');
+  });
 
-test('The first course to have a property of Title', () => {
-  expect(data[0]).toHaveProperty('views', '31,266');
+  test('The first course to have a property of Title', () => {
+    expect(data[0]).toHaveProperty('views', '31,266');
+  });
 });
